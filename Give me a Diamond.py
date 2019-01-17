2393a24aa3cb45a1341850547b3eeac0a6c59c82
Nicolas Molina Monroy <hi@nicobytes.com>
2019-01-17 16:20:01 -0500

    Update Give me a Diamond.py

--- Give me a Diamond.py
+++ Give me a Diamond.py
@@ -1,35 +1,15 @@
-
-
-
 def diamond(n):
-    lower_space = ''
-    top_space =  '' 
-    parameter = n    
-    lower_pyramid = ''
-    top_pyramid = ''
-    
-   
-    for z in range(1,n,2):
-        for q in range(1,parameter,2):
-            top_space += ' '
-
-        parameter-=2 
-        top_pyramid += '{}{}\n'.format(top_space , '*'*z )
-        top_space = ''
-
-    for p in range(n,0,-2):
-        if p == n :
-            lower_pyramid = '{}\n'.format('*'*p )
-        else:
-            lower_space += ' '
-            
-            lower_pyramid += '{}{}\n'.format(lower_space , '*'*p )
-           
-
-    diamante = top_pyramid + lower_pyramid
-
-    return None if  n < 0 or n % 2 == 0 else diamante
-
+	if n % 2 == 0 or n < 0: return None
+	x = 1
+	result = ""
+	for f in range(0, n):
+		spaces = (n - x) / 2
+		result += (" " * spaces) + ("*" * x) + "\n"
+		if f < n / 2:
+			x+=2
+		else:
+			x-=2
+	return result
 
 print(diamond(1))
 print(diamond(0))
